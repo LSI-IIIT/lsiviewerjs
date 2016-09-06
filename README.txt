52fdfc8c6a0bce5e0d98f1523efa3014d572d43a
Prasanth Rajan <prasanth@Abhimanyus-iMac.local>
2016-09-06 14:01:43 +0530

    User authentication system added, commit left on prasanths system

---
+++ README.txt
@@ -0,0 +1,2 @@
+
+MongoDB Install : https://www.mongodb.com/dr/fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.2.9.tgz/download
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://Abhimanyus-iMac.local:27017/lsiviewerjs' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
